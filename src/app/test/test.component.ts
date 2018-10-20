7cd7e53c26699218468338e23bfa70250f3c79ae
Ádám Berki <adamberki97@gmail.com>
2018-10-20 11:37:14 +0200

    mainscreen design developement

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,51 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-template:`<h2 [ngStyle]="titleStyle"> Welcome {{siteUrl.toUpperCase()}} ! </h2>
-  <input [disabled]="isDisabled" [id]="myId" type="text" value="Adam">
-  <h2 [class]="specialClass">{{whaDayIsToday()}}</h2>
-  <h2 [style.color]="hasError ? 'red' : 'orange'">Name: {{titleInput.value}}</h2>
+selector: 'app-test',
+template:`<h2> Welcome {{siteUrl.toUpperCase()}} ! </h2>
+  <h2 [style.color]="hasError ? 'green' : 'orange'">Name: {{titleInput.value}}</h2>
   <input type="text"  #titleInput>
-  <button type="submit" (click) = 'addTodo(titleInput.value)'>Add</button>
+  <button type="submit" (click)="addTodo(titleInput.value)">Add</button>
   
   <p>
+    Please choose difficulity lvl: 
     <button (click)="onClick($event)">Hard</button>
+    or
     <button (click)="greeting='lvl easy choosen'">Easy</button>
     {{greeting}}
   </p>`,
-  styles: 
-  [`.text-success{
-      color:green;
-    }
-    .text-special{
-      font-style:italic;
-    }
-  `]
+  styles: []
 })
 export class TestComponent implements OnInit {
 
   public name = "Guests";
-  public day = "Friday"; 
   public siteUrl = window.location.href;
   public myId = "testId";
-  public isDisabled = false;
   public hasError = true;
-  public successClass = "text-success";
-  public specialClass="text-special";
-  public titleStyle = {
-    color: "blue",
-    fontStyle: "italic"
-  }
   public greeting = '';
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  whaDayIsToday(){
-    return "Today is " + this.day;
   }
 
   onClick(event){
