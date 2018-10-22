333a3374e311ce9d839d1067e1b76a067b754279
Ádám Berki <adamberki97@gmail.com>
2018-10-22 07:58:56 +0200

    Add button actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,40 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'VGB Co.';
+
+  visible_mainScreen=true;
+  visible_LoginBox=false;
+  visible_About=false;
+  visible_Ladeboard=false;
+
+  clickOnPlay(){
+    this.visible_LoginBox = true;
+    this.visible_About=false;
+    this.visible_mainScreen=false;
+    this.visible_Ladeboard=false;
+  }
+  
+  clickOnAbout(){
+    this.visible_About=true;
+    this.visible_LoginBox = false;
+    this.visible_mainScreen=false;
+    this.visible_Ladeboard=false;
+  }
+
+  clickOnLadeboard(){
+    this.visible_Ladeboard=true;
+    this.visible_LoginBox = false;
+    this.visible_About=false;
+    this.visible_mainScreen=false;
+  }
+
+  clickOnMainScreen(){
+    this.visible_mainScreen=true;
+    this.visible_Ladeboard=false;
+    this.visible_LoginBox = false;
+    this.visible_About=false;
+  }
+
 }
 
 
