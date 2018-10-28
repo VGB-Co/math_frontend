4f83222922b7f23d3d14e92be5adc8c6be6ba049
Ádám Berki <adamberki97@gmail.com>
2018-10-28 13:18:02 +0100

    Add Sign In onClick() action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,12 +31,16 @@ export class AppComponent {
   visible_LoginBox=false;
   visible_About=false;
   visible_Ladeboard=false;
+  visible_Questions=false;
+  value_userInput="";
+  value_passwordInput="";
 
   clickOnPlay(){
     this.visible_LoginBox = true;
     this.visible_About=false;
     this.visible_mainScreen=false;
     this.visible_Ladeboard=false;
+    this.visible_Questions=false;
   }
   
   clickOnAbout(){
@@ -44,6 +48,7 @@ export class AppComponent {
     this.visible_LoginBox = false;
     this.visible_mainScreen=false;
     this.visible_Ladeboard=false;
+    this.visible_Questions=false;
   }
 
   clickOnLadeboard(){
@@ -51,6 +56,7 @@ export class AppComponent {
     this.visible_LoginBox = false;
     this.visible_About=false;
     this.visible_mainScreen=false;
+    this.visible_Questions=false;
   }
 
   clickOnMainScreen(){
@@ -58,6 +64,21 @@ export class AppComponent {
     this.visible_Ladeboard=false;
     this.visible_LoginBox = false;
     this.visible_About=false;
+    this.visible_Questions=false;
+  }
+
+  clickOnSignIn(){
+    console.log("Works SI!");
+    this.value_userInput="WorksUser";
+    this.value_passwordInput="WorksPassword";
+    console.log(this.value_userInput);
+    console.log(this.value_passwordInput);
+    this.visible_LoginBox = false;
+    this.visible_Questions=true;
+  }
+
+  clickOnSubmitAnswer(){
+    console.log("Works SA!")
   }
 
 }
