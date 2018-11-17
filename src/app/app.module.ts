15744957ee71f477c093ec2c244770955a545a12
Ádám Berki <adamberki97@gmail.com>
2018-11-17 12:13:15 +0100

    Add btn css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,10 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestClient } from './Mocky/rest.client';
 
-import {AngularFireModule} from '@angular/fire';
+import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,8 +20,12 @@ import { environment } from '../environments/environment';
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [RestClient],
   bootstrap: [AppComponent]
