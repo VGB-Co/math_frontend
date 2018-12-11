b5f72cb34ea92eb4de8002390bffe799b4496658
Ádám Berki <adamberki97@gmail.com>
2018-12-11 14:25:53 +0100

    Add valid table depends on the choosen diff level

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material';
+import {RestClient} from '../api.service';
+import {User} from '../Models/user';
 
 @Component({
   selector: 'app-leaderboard',
@@ -8,26 +10,46 @@ import { MatTabChangeEvent } from '@angular/material';
 })
 export class LeaderboardComponent implements OnInit {
 
-  constructor() {}
+  constructor(private _restClient: RestClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {this.getTopListFor(0)}
+
+  public topListUsers = Array<User>();
 
   public tabChanged(tabChangeEvent: MatTabChangeEvent): void {
       console.log(tabChangeEvent);
       if(tabChangeEvent.index==0){
         console.log('Easy leaderboard listed');
-        localStorage.setItem('chooseLeaderboardLevel', '0');
+        this.getTopListFor(0);
       }
       else{
         if(tabChangeEvent.index==1){
           console.log('Medium leaderboard listed');
-          localStorage.setItem('chooseLeaderboardLevel', '1'); 
+          this.getTopListFor(1);
         }
         else{
           console.log('Hard leaderboard listed');
-          localStorage.setItem('chooseLeaderboardLevel', '2'); 
+          this.getTopListFor(2);
         }
       }
+  }
+
+  public getTopListFor(level) {
+    this._restClient.getTopListFor(level).subscribe(
+    data => {
+              var users = new Array<User>();
+              data["users"].forEach(user => {
+                let curr_user = new User();
+                curr_user.name=user['name'];
+                curr_user.score=user['score'];
+                curr_user.time=user['time'];
+                users.push(curr_user);
+              });
+              this.topListUsers=users;
+            },
+    err => {console.error(err)},
+    () => console.log(this.topListUsers),
+  );
   }
 
 }
--- src/app/left-menu/left-menu.component.ts
+++ src/app/left-menu/left-menu.component.ts
@@ -27,6 +27,7 @@ export class LeftMenuComponent implements OnInit {
     console.log(localStorage.getItem('LoggedUser') + ' successfully logged out');
     localStorage.removeItem('activeToken');
     localStorage.removeItem('LoggedUser');
+    localStorage.removeItem('chooseLeaderboardLevel');
   }
 
 }
